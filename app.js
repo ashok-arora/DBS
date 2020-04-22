301539ee88df179886fe4178ca1d344c46c209a0
aniketsharma00411 <aniketsharma00411@gmail.com>
2020-04-22 16:49:12 +0530

    Multer

--- app.js
+++ app.js
@@ -1,34 +1,73 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
+var express = require("express");
+var multer = require("multer");
 const session = require("express-session");
-const bodyParser = require("body-parser");
-const app = express();
+var fs = require("fs");
+var app = express();
 var path = require("path");
-
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname + "/public")));
+const mysql = require("mysql");
 
 app.use(
   session({
     secret: "seCReT",
-
     resave: false,
-
     saveUninitialized: true,
-
-    cookie: { maxAge: 360000 },
+    cookie: { maxAge: 3600000 },
   })
 );
 
-app.use("/", require("./routes/index.js"));
+const connection = mysql.createConnection({
+  host: "localhost",
 
-app.use("/users", require("./routes/users.js"));
+  user: "root",
 
-app.get("*", (req, res) => {
-  res.redirect("/");
+  password: "aniket1234",
+
+  database: "image",
+});
+connection.connect((err) => {
+  if (err) console.log(err);
+
+  console.log("Database Connected!");
+});
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + "/uploads"); //you tell where to upload the files,
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + ".png");
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + " is starting ...");
+  },
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname + "/multer.html"));
+});
+
+app.post("/profile", upload.single("pic"), function (request, response) {
+  console.log(request.file);
+  //   const img = request.file;
+  //   console.log("1");
+  //   fs.readFile(img.path, (err, data) => {
+  //     console.log("1");
+  //     connection.query(
+  //       `INSERT INTO images(img) VALUES ?`,
+  //       [img],
+  //       (err, results) => {
+  //         console.log(err);
+  //       }
+  //     );
+  //     console.log("1");
+  //   });
+  //   return false;
 });
 
 const PORT = process.env.PORT || 5000;
