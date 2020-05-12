0cce3391f89bfac22d01f174d58e9c7b4be8d085
aniketsharma00411 <aniketsharma00411@gmail.com>
2020-05-13 00:28:54 +0530

    Removed incomplete graphs

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,6 @@ const mySqlConnection = require("../db/db");
 const bcrypt = require("bcrypt");
 const rp = require("request-promise");
 const $ = require("cheerio");
-var Chart = require("chart.js");
-// const url =
-//   "https://www.iiitm.ac.in/index.php/en/component/content/category/79-latest-news?Itemid=437";
 var user,
   m_name = "",
   assignment_number = "";
@@ -108,223 +105,13 @@ function news(request, response) {
       newsHead = $(".introtext > h3 > span", html).text();
       newsHead = newsHead.substring(0, 150);
       newsHead += "...";
-      attendanceChartsData(request, response);
+      callRender(request, response);
     })
     .catch(function (err) {
       newsHead = "No News Available.";
-      attendanceChartsData(request, response);
+      callRender(request, response);
     });
 }
-
-let attendanceChartsList = [];
-let attendanceSubjects = [];
-function attendanceChartsData(request, response) {
-  mySqlConnection.query(
-    "SELECT * FROM attendance WHERE roll_no = ?",
-    [user.roll_no],
-    (err, rows) => {
-      if (err) response.status(500).send(err);
-      else {
-        let temp = new Object();
-        let attendanceCharts = [];
-        for (row of rows) {
-          temp = new Object();
-          temp["label"] = "Present";
-          temp["y"] = row.attendance;
-          attendanceCharts = [];
-          attendanceCharts.push(temp);
-          temp = new Object();
-          mySqlConnection.query(
-            "SELECT * FROM subject WHERE subject_code = ?",
-            [row.subject_code],
-            (err, newRows) => {
-              if (err) response.status(500).send(err);
-              else {
-                temp["y"] = newRows[0].faculty_attendance - row.attendance;
-                temp["label"] = "Absent";
-                attendanceSubjects.push(newRows[0].subject_name);
-                attendanceCharts.push(temp);
-                attendanceChartsList.push(attendanceCharts);
-                if (row == rows[rows.length - 1])
-                  theoryChartsData(request, response);
-              }
-            }
-          );
-        }
-      }
-    }
-  );
-}
-
-let gradesChartsList = [];
-let gradesSubjects = [];
-function theoryChartsData(request, response) {
-  mySqlConnection.query(
-    "SELECT * FROM t_grades WHERE roll_no = ?",
-    [user.roll_no],
-    (err, rows) => {
-      if (err) response.status(500).send(err);
-      else {
-        let temp = new Object();
-        let gradesCharts = [];
-        for (row of rows) {
-          temp = new Object();
-          temp["y"] =
-            row.assignment_marks +
-            row.attendance_marks +
-            row.minor1 +
-            row.minor2 +
-            row.major;
-          gradesCharts = [];
-          gradesCharts.push(temp);
-          temp = new Object();
-          mySqlConnection.query(
-            "SELECT * FROM max_t_grades WHERE subject_code = ?",
-            [row.subject_code],
-            (err, newRows) => {
-              if (err) response.status(500).send(err);
-              else {
-                temp["y"] =
-                  newRows.assignment_marks +
-                  newRows.attendance_marks +
-                  newRows.minor1 +
-                  newRows.minor2 +
-                  newRows.major -
-                  (row.assignment_marks +
-                    row.attendance_marks +
-                    row.minor1 +
-                    row.minor2 +
-                    row.major);
-                gradesSubjects.push(newRows[0].subject_name);
-                gradesCharts.push(temp);
-                gradesChartsList.push(gradesCharts);
-                if (row == rows[rows.length - 1])
-                  labChartsData(request, response);
-              }
-            }
-          );
-        }
-      }
-    }
-  );
-}
-
-function labChartsData(request, response) {
-  mySqlConnection.query(
-    "SELECT * FROM l_grades WHERE roll_no = ?",
-    [user.roll_no],
-    (err, rows) => {
-      if (err) response.status(500).send(err);
-      else {
-        let temp = new Object();
-        let gradesCharts = [];
-        for (row of rows) {
-          temp = new Object();
-          temp["y"] =
-            row.assignment_marks +
-            row.attendance_marks +
-            row.mid_sem +
-            row.major;
-          gradesCharts = [];
-          gradesCharts.push(temp);
-          temp = new Object();
-          mySqlConnection.query(
-            "SELECT * FROM max_l_grades WHERE subject_code = ?",
-            [row.subject_code],
-            (err, newRows) => {
-              if (err) response.status(500).send(err);
-              else {
-                temp["y"] =
-                  newRows.assignment_marks +
-                  newRows.attendance_marks +
-                  newRows.mid_sem +
-                  newRows.major -
-                  (row.assignment_marks +
-                    row.attendance_marks +
-                    row.mid_sem +
-                    row.major);
-                gradesSubjects.push(newRows[0].subject_name);
-                gradesCharts.push(temp);
-                gradesChartsList.push(gradesCharts);
-                if (row == rows[rows.length - 1])
-                  // attendanceChart(request, response);
-                  callRender(request, response);
-              }
-            }
-          );
-        }
-      }
-    }
-  );
-}
-
-var attendanceChartsRender = [];
-// function attendanceChart(response, render) {
-//   let v = 0;
-//   let i = "chartContainer" + v.toString();
-//   for (data of attendanceChartsList) {
-//     window.onload = function () {
-//       var chart = new CanvasJS.Chart(i, {
-//         theme: "dark2", // "light1", "light2", "dark1"
-//         animationEnabled: false,
-//         height: 120,
-//         width: 170,
-//         title: {
-//           text: attendanceSubjects[subjectIterator],
-//         },
-//         data: [
-//           {
-//             type: "doughnut",
-//             dataPoints: data,
-//           },
-//         ],
-//         options: {
-//           //   responsive: false//,
-//           maintainAspectRatio: false,
-//         },
-//       });
-//       attendanceChartsRender.push(chart);
-//       if (data == attendanceChartsList[attendanceChartsList.length - 1])
-//         gradesChart(request, response);
-//     };
-//     v++;
-//     i = "chartContainer" + v.toString();
-//   }
-// }
-
-var gradesChartRender = [];
-// function gradesChart(response, render) {
-//   let w = 0;
-//   let j = "gradesContainer" + w.toString();
-//   for (data of gradesChartsList) {
-//     window.onload = function () {
-//       var chart = new CanvasJS.Chart(j, {
-//         theme: "dark2", // "light1", "light2", "dark1"
-//         animationEnabled: false,
-//         height: 120,
-//         width: 170,
-//         title: {
-//           text: gradesSubjects[w - 1],
-//         },
-//         data: [
-//           {
-//             type: "doughnut",
-//             dataPoints: data,
-//           },
-//         ],
-//         options: {
-//           //   responsive: false//,
-//           maintainAspectRatio: false,
-//         },
-//       });
-//       gradesChartRender.push(chart);
-//       if (data == gradesChartsList[gradesChartsList.length - 1])
-//         callRender(request, response);
-//     };
-//     w++;
-//     j = "gradesContainer" + w.toString();
-//   }
-// }
 
 function callRender(request, response) {
   response.redirect("/users/student_portal");
@@ -345,14 +132,6 @@ router.get("/student_portal", (request, response) => {
       photo: user.photo,
       batch_code: user.batch_code,
       newsHead: newsHead,
-      attendanceChartData: attendanceChartsList,
-      attendanceSubjects: attendanceSubjects,
-      subjectIterator: 1,
-      gradesChartData: gradesChartsList,
-      gradesSubjects: gradesSubjects,
-      gradesIterator: 1,
-      attendanceChartsRender: attendanceChartsRender,
-      gradesChartRender: gradesChartRender,
     });
   }
 });
